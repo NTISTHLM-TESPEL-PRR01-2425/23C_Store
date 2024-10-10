01117f8b58a66e9484a1a7416c9b1c7d1b4f2cab
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2024-10-10 12:57:55 +0200

    Initial commit

---
+++ Store/Program.cs
@@ -0,0 +1,42 @@
+﻿int i = 0;
+
+while (i < 10)
+{
+  Console.WriteLine("hej");
+  i++;
+}
+
+int num = 996;
+
+for (int j = 0; j < num; j++)
+{
+
+}
+
+
+// int ageNum = 0;
+// bool success = false;
+
+// while (success == false)
+// {
+//   Console.WriteLine("Hur gammal är du?");
+//   string age = Console.ReadLine();
+//   success = int.TryParse(age, out ageNum);
+
+//   if (success == false)
+//   {
+//     Console.WriteLine("Du måste skriva en siffra, dumhuvve");
+//   }
+// }
+
+// if (ageNum < 18)
+// {
+//   Console.WriteLine("Nä du är för ung");
+// }
+// else
+// {
+//   Console.WriteLine("Okej, välkommen");
+// }
+
+
+// Console.ReadLine();
